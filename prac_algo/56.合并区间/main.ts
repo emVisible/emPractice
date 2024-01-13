232201c55482754f0054b3cde13fe311f285dc46
young <1619346963@qq.com>
2024-01-13 23:55:20 +0800

    20240113

--- prac_algo/56.合并区间/main.ts
+++ prac_algo/56.合并区间/main.ts
@@ -1,9 +1,18 @@
 function merge(intervals: number[][]): number[][] {
-  const res = [] as number[]
-  for (let i = 0; i < intervals.length - 1; i++) {
-    
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  const merged = [];
+  for (const interval of intervals) {
+    // 如果列表为空，或者当前区间与上一区间不重合，直接添加
+    if (!merged.length || merged[merged.length - 1][1] < interval[0]) {
+      merged.push(interval);
+    } else {
+      // 否则的话，我们就可以与上一区间进行合并
+      merged[merged.length - 1][1] = Math.max(merged[merged.length - 1][1], interval[1]);
+    }
   }
-  return res
+
+  return merged;
 };
 const res = merge([[1, 3], [2, 6], [8, 10], [15, 18]])
 console.log('res', res)
